087ae89abf83256a6ad6be52b9dcf8deebe61029
Senthilvel <t.senthilvel@hotmail.com>
2023-02-23 12:17:13 +0530

    added factory method

--- AcquireX/Program.cs
+++ AcquireX/Program.cs
@@ -1,8 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using AcquireX.Core.Contracts;
+using AcquireX.Core.Enum;
 using AcquireX.Core.Services;
 using AcquireX.Core.Utility;
- 
+
 
 string clientId = "candidate1";
 string clientSecret = "X01BvqCjkbFpsw5d1gAChjg1RrQc3c2q";
@@ -11,17 +12,25 @@ string rsHughesServiceUrl = "https://dkh-c-testing-api.staging.dkhdev.com/produc
 string bannerServiceUrl = "https://dkh-c-testing-api.staging.dkhdev.com/products/xml";
 
 var oauthService = OAuthService.GetInstance(clientId, clientSecret, tokenEndpoint);
-var rsHughesDataSource = new RSHughesDataSource(oauthService, rsHughesServiceUrl);
-var products = await rsHughesDataSource.GetProductsAsync();
-var bannerDataSource = new BannerDataSource(oauthService, bannerServiceUrl);
-var bannerDataSourceProducts = await bannerDataSource.GetProductsAsync();
+var dataSourceFactory = new DataSourceFactory(oauthService);
+
+var bannerDataSource = dataSourceFactory.CreateDataSource(DataSourceType.Banner, bannerServiceUrl);
+var bannerProducts = await bannerDataSource.GetProductsAsync();
+
+var rsHughesDataSource = dataSourceFactory.CreateDataSource(DataSourceType.RSHughes, rsHughesServiceUrl);
+var rsHughesProducts = await rsHughesDataSource.GetProductsAsync();
+ 
 
 UPCProductMatcher matcher = new UPCProductMatcher();
-var matchedProd = matcher.Match(products, bannerDataSourceProducts);
 
-foreach (var product in matchedProd)
+if (rsHughesProducts.Any() && bannerProducts.Any())
 {
+    var matchedProd = matcher.Match(rsHughesProducts, bannerProducts);
+    if (matchedProd.Any())
+        foreach (var product in matchedProd)
+        {
 
-    Console.WriteLine($"{product.Upc} {product.ItemCode}");
+            Console.WriteLine($"{product.Upc} {product.ItemCode}");
+        }
 }
 Console.ReadLine();
